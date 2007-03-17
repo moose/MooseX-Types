e9c7cea885688c43a2f3114e6f89db3c4c47caec
phaylon <phaylon@3efe9002-19ed-0310-8735-a98156148065>
2007-03-17 16:48:56 +0000

    TypeLibrary now uses list_all_builtin_type_constraints (phaylon)

--- lib/MooseX/TypeLibrary/Moose.pm
+++ lib/MooseX/TypeLibrary/Moose.pm
@@ -9,7 +9,7 @@ use namespace::clean;
 # all available builtin types as short and long name
 my %BuiltIn_Storage 
   = map { ($_) x 2 } 
-    Moose::Util::TypeConstraints->list_all_type_constraints;
+    Moose::Util::TypeConstraints->list_all_builtin_type_constraints;
 
 # use prepopulated builtin hash as type storage
 sub type_storage { \%BuiltIn_Storage }

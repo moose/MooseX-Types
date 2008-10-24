f235df49d0d645313d5afeecf7ace023bb6f78ad
John Napiorkowski <jjnapiork@cpan.org>
2008-10-24 20:51:15 +0000

    merged in some updates from trunk

--- t/lib/DecoratorLibrary.pm
+++ t/lib/DecoratorLibrary.pm
@@ -1,3 +1,4 @@
+<<<<<<< .working
 package DecoratorLibrary;
 
 use MooseX::Types::Moose qw( Str ArrayRef HashRef Int );
@@ -82,3 +83,75 @@ enum Jobs,
     (qw/Programming Teaching Banking/);
     
 1;
+=======
+package DecoratorLibrary;
+
+use MooseX::Types::Moose qw( Str ArrayRef HashRef Int );
+use MooseX::Types
+    -declare => [qw(
+        MyArrayRefBase
+        MyArrayRefInt01
+        MyArrayRefInt02
+        MyHashRefOfInts
+        MyHashRefOfStr
+        StrOrArrayRef
+        AtLeastOneInt
+        Jobs
+        SubOfMyArrayRefInt01
+        BiggerInt
+    )];
+
+subtype MyArrayRefBase,
+    as ArrayRef;
+    
+coerce MyArrayRefBase,
+    from Str,
+    via {[split(',', $_)]};
+    
+subtype MyArrayRefInt01,
+    as ArrayRef[Int];
+
+subtype BiggerInt,
+    as Int,
+    where {$_>10};
+    
+subtype SubOfMyArrayRefInt01,
+    as MyArrayRefInt01[BiggerInt];
+
+coerce MyArrayRefInt01,
+    from Str,
+    via {[split('\.',$_)]},
+    from HashRef,
+    via {[sort values(%$_)]};
+    
+subtype MyArrayRefInt02,
+    as MyArrayRefBase[Int];
+    
+subtype MyHashRefOfInts,
+    as HashRef[Int];
+    
+subtype MyHashRefOfStr,
+    as HashRef[Str];
+
+coerce MyArrayRefInt02,
+    from Str,
+    via {[split(':',$_)]},
+    from MyHashRefOfInts,
+    via {[sort values(%$_)]},
+    from MyHashRefOfStr,
+    via {[ sort map { length $_ } values(%$_) ]},
+    from HashRef[ArrayRef],
+    via {[ sort map { @$_ } values(%$_) ]};
+
+subtype StrOrArrayRef,
+    as Str|ArrayRef;
+
+subtype AtLeastOneInt,
+    as ArrayRef[Int],
+    where { @$_ > 0 };
+    
+enum Jobs,
+    (qw/Programming Teaching Banking/);
+    
+1;
+>>>>>>> .merge-right.r6407

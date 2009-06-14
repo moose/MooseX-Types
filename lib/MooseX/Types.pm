4e6dc81d073596e98bdf19f5884ee2397da31b5e
Rafael Kitover <rkitover@cpan.org>
2009-06-14 14:42:04 -0700

    fix mixing of typelibs with regular Moose classes

--- lib/MooseX/Types.pm
+++ lib/MooseX/Types.pm
@@ -20,7 +20,7 @@ use Scalar::Util                      'reftype';
 use namespace::clean -except => [qw( meta )];
 
 use 5.008;
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 my $UndefMsg = q{Action for type '%s' not yet defined in library '%s'};
 
 =head1 SYNOPSIS
@@ -374,8 +374,8 @@ sub type_export_generator {
     
     ## Return an anonymous subroutine that will generate the proxied type
     ## constraint for you.
-    
-    return subname $name => sub {
+
+    return subname "__ANON__::$name" => sub {
         my $type_constraint = $class->create_base_type_constraint($name);
 
         if(defined(my $params = shift @_)) {

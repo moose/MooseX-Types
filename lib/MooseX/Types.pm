1150ce7231722f60b82d2b8399cd16671102f26d
Rafael Kitover <rkitover@cpan.org>
2009-06-14 17:39:26 -0700

    rename exports __TYPE__:: instead of __ANON__:: because stevan likes that better :)

--- lib/MooseX/Types.pm
+++ lib/MooseX/Types.pm
@@ -375,7 +375,7 @@ sub type_export_generator {
     ## Return an anonymous subroutine that will generate the proxied type
     ## constraint for you.
 
-    return subname "__ANON__::$name" => sub {
+    return subname "__TYPE__::$name" => sub {
         my $type_constraint = $class->create_base_type_constraint($name);
 
         if(defined(my $params = shift @_)) {

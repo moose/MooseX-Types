58bf58dcf21a1ac8054831ec2c6e328241b6ffa6
Rafael Kitover <rkitover@cpan.org>
2009-06-29 10:52:03 -0700

    new release with updated M::I

--- lib/MooseX/Types.pm
+++ lib/MooseX/Types.pm
@@ -20,7 +20,7 @@ use Scalar::Util                      'reftype';
 use namespace::clean -except => [qw( meta )];
 
 use 5.008;
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 my $UndefMsg = q{Action for type '%s' not yet defined in library '%s'};
 
 =head1 SYNOPSIS
